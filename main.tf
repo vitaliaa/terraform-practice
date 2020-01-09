be53593152d49c962d17d70240b23e834c0acebb
Vitalii Orlovskyi <bitali.94@mail.ru>
2020-01-09 23:10:08 +0200

    Update syntax of variable

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
 provider "aws" {
-  region     = "${var.aws_region}"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 
 resource "aws_instance" "test" {
-  ami                    = "ami-02df9ea15c1778c9c"
+  ami                    = var.aws_amis
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sec_group.id]
   key_name               = "terraform_ec2_key"
 
   tags = {
-    Name = "Name of instance"
+    Name = var.name_of_instance
   }
 }
 
 resource "aws_key_pair" "terraform_ec2_key" {
   key_name   = "terraform_ec2_key"
-  public_key = "${file("../.ssh/id_rsa.pub")}"
+  public_key = file("../.ssh/id_rsa.pub")
 }
 
 resource "aws_security_group" "sec_group" {
   name        = "HTTP-SSH"
-  description = "HTTP-SSH"
+  description = "Access-HTTP-SSH"
 
   ingress {
     from_port   = 80
@@ -32,15 +32,12 @@ resource "aws_security_group" "sec_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-
 
   egress {
     from_port   = 0
--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
-variable "ami" {
-  description = "ami-02df9ea15c1778c9c"
+variable "aws_amis" {
+  default = "ami-02df9ea15c1778c9c"
+}
 
+variable "name_of_instance" {
+	default = "Ubuntu_server_18.04"
 }
