45bcf6b1f55395f5675d949a86da4e09506564df
Vitalii Orlovskyi <bitali.94@mail.ru>
2020-01-13 19:00:37 +0200

    Add aws_region

--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
+variable "aws_region" {
+  description = "AWS region to launch servers"
+  default     = "eu-west-1"
+}
+
 variable "aws_amis" {
   default = "ami-02df9ea15c1778c9c"
 }
 
 variable "name_of_instance" {
-	default = "Ubuntu_server_18.04"
+  default = "Ubuntu_server_18.04"
 }
